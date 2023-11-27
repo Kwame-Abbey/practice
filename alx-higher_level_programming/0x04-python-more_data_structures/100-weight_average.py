88b750195edba5b41a1ebba1da464a8acf8acdba
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-11-27 16:16:10 +0000

    square matrix and weight average

---
+++ alx-higher_level_programming/0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def weight_average(my_list=[]):
+    if my_list:
+        numerator = 0
+        denominator = 0
+        for tup in my_list:
+            numerator += tup[0] * tup[1]
+            denominator += tup[1]
+        average = numerator / denominator
+    return average
