a89dda856e148da95013b5099bb41747aaddbf65
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-12-19 16:02:44 +0000

    More Sql queries

---
+++ alx-higher_level_programming/0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,7 @@
+-- Lists all records of the second_table
+-- Displays both the score and the name
+-- Ordered by the score (top first)
+
+SELECT score, name 
+    FROM second_table
+        ORDER BY score DESC;
---
+++ alx-higher_level_programming/0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- Insert a new row in table 'first_table'
+INSERT INTO first_table
+VALUES (89, 'Best School');
