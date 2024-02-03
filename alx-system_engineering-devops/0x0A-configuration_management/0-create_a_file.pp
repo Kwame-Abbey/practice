ddf5c3e0ebd33d36d39721c9e74bf02aac372bed
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2024-02-03 17:03:30 +0000

    install package

--- alx-system_engineering-devops/0x0A-configuration_management/0-create_a_file.pp
+++ alx-system_engineering-devops/0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # creates a file in '/tmp'
 
-file { '/tmp/school':
-  'ensure': file,
-  'mode': '0644'
-  'content': 'I love Puppet',
-  'owner': 'www-data',
-  'group': 'www-data'
+file { '/tmp/school': 
+  'path'    => '/tmp/school',
+  'mode'    => '0644',
+  'content' =>'I love Puppet',
+  'owner'   => 'www-data',
+  'group'   =>'www-data'
 }
