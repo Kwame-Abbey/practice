4afba3abdb7846188863834b05b75d73fa307dc6
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2024-02-06 13:23:12 +0000

    More functions and nested loops in C

---
+++ alx-low_level_programming/0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to check
+ *
+ * Return: 1 if c is uppercase or 0
+ */
+
+int _isupper(int c)
+{
+	return ((c >= 'A') && (c <= 'Z'));
+}
