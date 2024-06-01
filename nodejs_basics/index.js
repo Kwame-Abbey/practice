4b03afd552301d21429fc421e2c5878439795136
Kwame-Abbey <constancekwameabbey@gmail.com>
2024-06-01 15:51:29 +0100

    Node basics

---
+++ nodejs_basics/index.js
@@ -0,0 +1,10 @@
+import getPost, { getPostLength } from "./postController.js";
+
+console.log(getPost())
+console.log(getPostLength())
+
+
+// const {generateRandomNumber, celsiusToFahrenheit} = require('./utils')
+
+// console.log(`random Number: ${generateRandomNumber()}`)
+// console.log(`Fahrenheit: ${celsiusToFahrenheit(0)}`)
