949ad5b9ad6294dab32ae6ee0d4a7b6b27162398
Kwame-Abbey <constancekwameabbey@gmail.com>
2024-08-27 09:40:14 +0100

    Classes and objects

--- alx-revision/alx-higher_level_programming/0x05-python-exceptions/2-safe_print_list_integers.py
+++ alx-revision/alx-higher_level_programming/0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,6 +9,5 @@ def safe_print_list_integers(my_list=[], x=0):
         return nb
     except (ValueError, TypeError):
         s = 1
-        while s == 1:
-            s == 0
-            continue
+    if s = 1:
+        continue
